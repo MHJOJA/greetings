df9e5d3e46a6dc0cd34c73097771fcafe038f801
JodyFortuin <jodifortuin@gmail.com>
2020-10-30 10:16:42 +0200

    initial commit

--- greet.js
+++ greet.js
@@ -29,9 +29,9 @@ module.exports = function greet(pool) {
     async function setAnUpdate(name) {
         const setName = await pool.query('SELECT names FROM greet WHERE names= $1', [name])
         if (setName.rowCount === 0) {
-            storeNames(name)
+          await  storeNames(name)
         }
-        updateNames(name)
+        await   updateNames(name)
     }
 
 
