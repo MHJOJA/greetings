3374bf22c4e644db6d50107a470fd1e47b66274f
JodyFortuin <jodifortuin@gmail.com>
2020-11-10 11:04:55 +0200

    initial commit

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -1,10 +1,28 @@
 const assert = require('assert')
-let  greet = requie('./greet')
+let  Greet = require('../greet')
+const greet = Greet()
+const pg = require('pg')
+const Pool = pg.Pool;
 
-describe('the greet application',async function(){
-    let
+// we are using a special test database for the tests
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/my_greetings';
+const pool = new Pool({
+    connectionString
+})
+//const greetings = greet(pool)
+describe('The greetings application', async function () {
+
+    beforeEach(async function () {
+        // clean the tables before each test run
+        await pool.query("delete from greet;");
+
+    });
     
 })
 it('should be able to greet Thabo in Xhosa',function(){
-    assert.equal('Molo Thabo',greet.greeted('Thabo','Xhosa'))
+    assert.equal('Molo, Thabo',greet.greeted('Thabo','xhosa'))
 })
+// it('should be able to greet Thabo in Xhosa',function(){
+//     var result =greet.greeted('Thabo','xhosa')
+//     assert.equal('Molo, Thabo',result)
+// })
